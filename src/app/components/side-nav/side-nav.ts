eda587caa89070964927281b227fb65e3849d8b7
amfajardoo <amfajardoo94@gmail.com>
2025-09-21 13:24:07 -0500

    feat: enhance side navigation and dashboard layout with collapsible functionality and improved routing

--- src/app/components/side-nav/side-nav.ts
+++ src/app/components/side-nav/side-nav.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface NavItem {
+	name: string;
+	icon: string;
+	route: string;
+}
 
 @Component({
-  selector: 'app-side-nav',
-  imports: [],
-  templateUrl: './side-nav.html',
-  styleUrl: './side-nav.css'
+	selector: 'app-side-nav',
+	standalone: true,
+	imports: [RouterLink, RouterLinkActive],
+	templateUrl: './side-nav.html',
+	styleUrl: './side-nav.css',
 })
 export class SideNav {
+	isCollapsed = input.required<boolean>();
 
+	navItems = signal<NavItem[]>([
+		{ name: 'Transactions', icon: 'exchange', route: './transactions' },
+		{ name: 'Categories', icon: 'folder', route: './categories' },
+		{ name: 'Goals', icon: 'target', route: './goals' },
+	]);
 }
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNav } from '@components/side-nav/side-nav';
 
 @Component({
+	standalone: true,
 	styleUrl: './dashboard.css',
 	templateUrl: './dashboard.html',
 	imports: [RouterOutlet, SideNav],
 })
-export default class Dashboard {}
+export default class Dashboard {
+	isMobileMenuOpen = signal(true);
+	isCollapsed = signal(false);
+
+	openMobileMenu() {
+		this.isMobileMenuOpen.set(true);
+	}
+
+	toggleCollapse() {
+		this.isCollapsed.update((v) => !v);
+	}
+
+	closeMobileMenu() {
+		this.isMobileMenuOpen.set(false);
+	}
+}
